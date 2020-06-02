37c963acda1b37e0792ea79f48a20c7e434daae1
Andrew Reusch <areusch@octoml.ai>
2020-06-01 19:31:59 -0700

    checkpoint

--- src/runtime/crt/crt_backend_api.c
+++ src/runtime/crt/crt_backend_api.c
@@ -23,6 +23,7 @@
 #include <string.h>
 #include <tvm/runtime/c_backend_api.h>
 #include <tvm/runtime/crt/memory.h>
+#include "packed_func.h"
 
 void* TVMBackendAllocWorkspace(int device_type, int device_id, uint64_t nbytes, int dtype_code_hint,
                                int dtype_bits_hint) {
