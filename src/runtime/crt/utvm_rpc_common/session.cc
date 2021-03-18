4d2de915e15dc92f1531dd725f27ca3711520645
Mehrdad Hessar <mehrdad.hessar@gmail.com>
2021-03-17 22:12:29 -0700

    add crt flag

--- src/runtime/crt/utvm_rpc_common/session.cc
+++ src/runtime/crt/utvm_rpc_common/session.cc
@@ -62,9 +62,16 @@ tvm_crt_error_t Session::SendInternal(MessageType message_type, const uint8_t* m
 
 tvm_crt_error_t Session::StartMessage(MessageType message_type, size_t message_size_bytes) {
   SessionHeader header{session_id_, message_type};
-  if (message_type == MessageType::kLog || message_type == MessageType::kTerminateSession) {
+  if (message_type == MessageType::kLog) {
     header.session_id = 0;
   }
+#ifdef TVM_CRT_ERROR_MODULE_ENABLE
+  if (message_type == MessageType::kTerminateSession) {
+    header.session_id = 0;
+  }
+#endif
+
+
 
   tvm_crt_error_t to_return = framer_->StartPacket(message_size_bytes + sizeof(SessionHeader));
   if (to_return != 0) {
@@ -153,8 +160,12 @@ void Session::SessionReceiver::PacketDone(bool is_valid) {
     case MessageType::kTerminateSession:
       if (session_->state_ == State::kSessionEstablished) {
         session_->state_ = State::kNoSessionEstablished;
+      #ifdef TVM_CRT_ERROR_MODULE_ENABLE
         session_->message_received_func_(session_->message_received_func_context_,
                                          header.message_type, session_->receive_buffer_);
+      #else
+        session_->OnSessionTerminatedMessage();
+      #endif
       }
       session_->receive_buffer_has_complete_message_ = false;
       break;
